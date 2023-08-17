865fb8dc01c3f8f9378feb6fa7b8def269013e75
shahina42 <141030593+shahina42@users.noreply.github.com>
2023-08-17 14:37:51 -0700

    update

---
+++ shapes.js
@@ -0,0 +1,41 @@
+var btn = document.getElementById("button");
+var circle = document.getElementById("circle");
+var square = document.getElementById("square");
+var rect = document.getElementById("rectangle");
+var box = document.getElementById("box");
+var num = document.getElementById("number");
+var temp=1
+
+btn.onclick = function name() {
+    for (let i = 0; i < num.value; i++) {
+
+        if (circle.checked) {
+            var el = document.createElement("div");
+            el.classList.add("circle")
+            el.innerText = temp
+            temp = temp + 1
+            box.appendChild(el)
+
+        }
+
+
+        else if (square.checked) {
+            var el = document.createElement("div");
+            el.classList.add("square")
+            el.innerText = temp
+            temp = temp + 1
+            box.appendChild(el)
+
+
+        }
+        else if (rect.checked) {
+            var el = document.createElement("div");
+            el.classList.add("rectangle")
+            el.innerText = temp
+            temp = temp + 1
+            box.appendChild(el)
+
+
+        }
+    }
+}
